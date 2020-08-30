3f122aeaf918e8d2ce660472d8390e67069c0b8a
jnr <13692453351@163.com>
2020-08-30 09:06:32 +0800

    修改ip@JNR

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="default"
+                @click="showPopup">默认按钮</van-button>
+    <van-button type="primary"
+                @click="showPopup">主要按钮</van-button>
+    <van-button type="info"
+                @click="showPopup">信息按钮</van-button>
+    <van-button type="warning"
+                @click="showPopup">警告按钮</van-button>
+    <van-button type="danger"
+                @click="showPopup">危险按钮</van-button>
+    <van-popup v-model="show">弹出层内容</van-popup>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    showPopup () {
+      this.show = true;
+    }
   }
+
 }
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+import {
+  Button
+} from 'vant';
+import {
+  Popup
+} from 'vant';
 
+Vue.use(Popup);
+Vue.use(Button)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
